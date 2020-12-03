53f73787278d917d8831ee989d93b2b96545a76c
zhangxuewei <1501311415@qq.com>
2020-12-03 20:19:02 +0800

    first

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+import editorDashboard from './editor'
+
+export default {
+  name: 'Dashboard',
+  components: { adminDashboard, editorDashboard },
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    console.log(this.$route.query.selected)
+    if (this.$route.query.selected.includes('a')) {
+      this.currentRole = 'editorDashboard'
+    } else if ((this.$route.query.selected.includes('b'))) {
+      this.currentRole = 'adminDashboard'
+    }
+  }
+}
+</script>
